46e1e8e271fb13f4e690c3cd49d04c43374cc09f
aethel <aethelbrihte@gmail.com>
2015-11-01 13:34:43 +0100

    setting one item's value

--- app/scripts/directives/scoreItem.js
+++ app/scripts/directives/scoreItem.js
@@ -1,18 +1,32 @@
 'use strict';
 
 angular.module('stApp').directive('scoreItem', function(){
+  
+  
   return {
     restrict:' E',
     templateUrl: '/scripts/directives/scoreItem.html',
     transclude: true,
     controller: function(){
-      console.log('is controll');
+     
     },
     controllerAs: 'ScoreItemCtrl',
     link: function(scope,element,attr){
+      scope.initPoints = scope.init.players;    
       element.bind('click', function(){
-        alert('ff');
+       
       });
+       
+      scope.addPoint = function(){
+  
+        var currentValue2 = element.next().text();
+        angular.element(this).next().text(scope.currentValue+1);
+      
+        console.log(currentValue2); 
+   
+//        scope.initPoints += 1;
+      };
+    
     }
   };
 });
