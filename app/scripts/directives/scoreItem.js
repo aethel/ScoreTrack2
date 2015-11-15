f37050df11516f9e3157f672ee073aabe1b9839d
aethel <aethelbrihte@gmail.com>
2015-11-15 22:48:17 +0100

    item directive

--- app/scripts/directives/scoreItem.js
+++ app/scripts/directives/scoreItem.js
@@ -1,33 +1,34 @@
 'use strict';
 
 angular.module('stApp').directive('scoreItem', function(){
-  
-  
-  return {
-    restrict:' E',
-    templateUrl: '/scripts/directives/scoreItem.html',
-    transclude: true,
-    controller: function(){
-     
-    },
-    controllerAs: 'ScoreItemCtrl',
-    link: function(scope,element,attr){
-      scope.initPoints = scope.init.points;    
-      element.bind('click', function(){
-       
-      });
-       
-      scope.addPoint = function(event){
-        
-        var elem = event.target;
-        var value = parseInt(elem.nextElementSibling.textContent);
-        
-        value += 1;
 
-   
+
+	return {
+		restrict:' E',
+		templateUrl: '/scripts/directives/scoreItem.html',
+		transclude: true,
+		controller: function(){
+
+		},
+		controllerAs: 'ScoreItemCtrl',
+		link: function(scope,element,attr){
+			scope.initPoints = scope.init.points;
+			element.bind('click', function(){
+
+			});
+
+			scope.addPoint = function(event){
+
+				var elem = event.target;
+				console.log(elem);
+				var value = parseInt(elem.nextElementSibling.textContent);
+				console.log(value);
+				value =(value+1).toString();
+
+
 //        scope.initPoints += 1;
-      };
-    
-    }
-  };
+			};
+
+		}
+	};
 });
