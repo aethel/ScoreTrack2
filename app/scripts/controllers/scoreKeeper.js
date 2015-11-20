ae40048530a8e3af6193becf2c7724acb079b6cc
aethel <mpankiewicz@infusion.com>
2015-11-20 13:55:38 +0100

    directive redo

--- app/scripts/controllers/scoreKeeper.js
+++ app/scripts/controllers/scoreKeeper.js
@@ -1,15 +1,33 @@
 'use strict';
 
 
-angular.module('stApp').controller('ScoreCtrl', function($scope, InitData, $compile){
+angular.module('stApp').controller('ScoreCtrl', function($scope, InitData){
    $scope.init = InitData.getValue();
-   var parent = angular.element(document.querySelector('.score-elements-list')); 
-   var elem = '<li><score-item></score-item></li>';
-   var start = $scope.init.players;
-   while(start > 0){
-     parent.append($compile(elem)($scope));     
-     start--;
-   }
-
   
 });
+
+
+angular.module('stApp').directive('scoreElement',function($compile){
+  return {
+    restrict: 'E',
+    templateUrl: '../scripts/directives/scoreElement.html',
+    controller: function($scope, InitData){
+      $scope.start = InitData.getValue();
+      var parent = angular.element(document.querySelector('.score-elements-list')); 
+      var elem = '<li><div><button>+</button>'+$scope.start.points+'<button>-</button></div></li>';
+      var start = $scope.init.players;
+      while(start > 0){
+        console.log(start);
+        parent.append(elem);
+        start--;
+
+      }  
+    },
+    controllerAs: 'scoreCtrl',
+    link: function(scope,element,attrs){
+
+    }
+  
+  };
+});
+
