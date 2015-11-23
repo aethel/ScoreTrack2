ee5fcafc1f11535ef61bce1e27409ee8c8debe56
aethel <mpankiewicz@infusion.com>
2015-11-23 13:22:58 +0100

    start button & from validation

--- app/scripts/controllers/scoreKeeper.js
+++ app/scripts/controllers/scoreKeeper.js
@@ -6,6 +6,7 @@ angular.module('stApp').controller('ScoreCtrl', function($scope, InitData, $comp
    var parent = angular.element(document.querySelector('.score-elements-list'));
     var elem = '<li><score-item></score-item></li>';
     var start = $scope.init.players;
+    $scope.init.players <= 8 ? start = $scope.init.players : start = 8;
     while(start > 0){
       parent.append($compile(elem)($scope));
       start--;
