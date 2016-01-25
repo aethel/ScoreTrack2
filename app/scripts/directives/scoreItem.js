58a4b74f6ca70b78f9fd2ee6afd388af5a205a79
aethel <aethelbrihte@gmail.com>
2016-01-25 19:49:37 +0100

    players initials

--- app/scripts/directives/scoreItem.js
+++ app/scripts/directives/scoreItem.js
@@ -5,9 +5,25 @@ angular.module('stApp').directive('scoreItem', function($timeout){
 	return {
 		restrict:' E',
 		templateUrl: '../../views/scoreItem.html',
-		transclude: true,
-		link: function(scope,element,attr){
+		requires: '^?ngModel',
+		link: function(scope,element,attr,ngModel){
 			scope.init.points <= 999 ? scope.initPoints = scope.init.points : scope.initPoints = 999;
+
+
+			var inputBox = element.find('input');
+			inputBox.bind('input',function(e){
+				console.log(e.target);
+				console.log(e.target.value);
+				console.log(scope.initials);
+			});
+
+
+//			scope.$apply(function(){
+//
+//				ngModel.$setViewValue('feck');
+//			});
+//			console.log(ngModel);
+
 		}
 	};
 });
