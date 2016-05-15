42b5cd000e9b854097f0f02c915b597c201c7a10
aethel <aethelbrihte@gmail.com>
2016-05-15 10:57:30 +0200

    button scaling

--- app/scripts/directives/scoreItem.js
+++ app/scripts/directives/scoreItem.js
@@ -9,17 +9,8 @@ angular.module('stApp').directive('scoreItem', function($timeout){
 		link: function(scope,element,attr,ngModel){
 			scope.init.points <= 999 ? scope.initPoints = scope.init.points : scope.initPoints = 999;
 
-//			var items = document.querySelectorAll('score-item');
-//			console.log(items.length);
-//			for(var i=0; i<items.length; i++){
-//				var item = items[i];
-//				var initialsBox = item.querySelector('.si__initials');
-//				console.log(initialsBox,'initbox');
-//				var content =+ i;
-//
-//				initialsBox.textContent = content;
-//			}
-
+			var initials = document.querySelectorAll('.si__initials');
+			console.log(initials[0].textContent.length);
 
 		}
 	};
