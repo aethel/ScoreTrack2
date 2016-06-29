cde5ca7f646a994b196efaa362232d6e4b789fdb
aethel <aethelbrihte@gmail.com>
2016-06-29 20:09:11 +0200

    code tidyup

--- app/scripts/directives/scoreItem.js
+++ app/scripts/directives/scoreItem.js
@@ -9,13 +9,9 @@ angular.module('stApp').directive('scoreItem', function($timeout){
 		link: function(scope,element,attr,ngModel){
 			scope.init.points <= 999 ? scope.initPoints = scope.init.points : scope.initPoints = 999;
 
-//			var initials = document.querySelectorAll('.si__initials'),
 			var initials = document.querySelectorAll('.si__initials-wrapper'),
 					list = document.querySelector('.score-elements-list');
 
-//			for(var i=0;i<initials.length;i++){
-//				initials[i].textContent = i+1
-//			}
 			for(var i=0;i<initials.length;i++){
 				initials[i].addEventListener('click',function($event){
 						$event.currentTarget.textContent = '';
